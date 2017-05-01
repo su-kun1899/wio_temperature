7e0f0d8e749f3e0859745d49d30907c5eb5d2eee
su-kun1899 <higedrum.coz@gmail.com>
2017-05-01 10:43:15 +0900

    Modify app.js

--- app.js
+++ app.js
@@ -2,21 +2,22 @@
 
 const axios = require('axios');
 const TOKEN = process.argv[2] || '';
-const BASE_PATH = `https://us.wio.seeed.io/v1/node`; // 自身のものをアプリで確認
+const BASE_PATH = `https://us.wio.seeed.io/v1/node`;
+const INTERVAL = 3000;
 
 /**
  * 温度取得
  */
- const getTemp = () => {
-     const ENDPOINT = `/GroveTempA0/temp`;
-     const BASE_URL = `${BASE_PATH}${ENDPOINT}?access_token=${TOKEN}`;
-     return axios.request({method: 'get', baseURL: BASE_URL}); // ここでは実行しない
- }
+const getTemp = () => {
+    const ENDPOINT = `/GroveTempA0/temp`;
+    const BASE_URL = `${BASE_PATH}${ENDPOINT}?access_token=${TOKEN}`;
+    return axios.request({method: 'get', baseURL: BASE_URL});
+}
 
 /**
  * 温度情報を文字列変換+Cを追加
  * @param {number} temp
- * @return {string}
+ * @param {string}
  */
 const temp2digit = (temp) =>{
     let temp_str = `${temp}`;
@@ -27,11 +28,45 @@ const temp2digit = (temp) =>{
     return `${temp_str}C`;
 }
 
-async function main(){ // async/awaitを使う
+/**
+ * :を表示/非表示
+ * @param {number} display - 0 or 1
+ */
+const fourDigitPoint = (display) => {
+    const ENDPOINT = `/Grove4DigitUART0/display_point/${display}`;
+    const BASE_URL = `${BASE_PATH}${ENDPOINT}?access_token=${TOKEN}`;
+    return axios.request({method: 'post', baseURL: BASE_URL});
+}
+
+/**
+ * ディスプレイに表示させる
+ * @param {number} start_pos
+ * @param {string} chars
+ */
+const fourDigit = (start_pos, chars) => {
+    const ENDPOINT = `/Grove4DigitUART0/display_digits/${start_pos}/${chars}`;
+    const BASE_URL = `${BASE_PATH}${ENDPOINT}?access_token=${TOKEN}`;
+    return axios.request({method: 'post', baseURL: BASE_URL});
+}
+
+/**
+ * メインの処理 (同期)
+ */
+async function main(){
    const res = await getTemp();
    const temp = res.data.temperature;
    const text = temp2digit(temp);
    console.log(text);
+   res = await fourDigit(0,text);
+   res = await fourDigitPoint(1);
 }
 
-main();
+/**
+ * 繰り返し実行
+ */
+setInterval(()=>{
+    main()
+    .catch((e)=>{
+        if(e.response) console.log(e.response);
+    })
+},INTERVAL);
